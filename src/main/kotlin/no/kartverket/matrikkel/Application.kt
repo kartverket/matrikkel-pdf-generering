8f8da2aac6c325626616d69c0004a9fafab61a27
fredrir <fredrik.hansteen@kartverket.no>
2026-07-01 15:58:57 +0200

    sette opp timeout på 10 sekunder

--- src/main/kotlin/no/kartverket/matrikkel/Application.kt
+++ src/main/kotlin/no/kartverket/matrikkel/Application.kt
@@ -1,12 +1,20 @@
 package no.kartverket.matrikkel
 
 import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.server.netty.*
 import no.kartverket.matrikkel.config.Configuration
 
+
 fun runApplication() {
     val config = Configuration()
-    val client = HttpClient()
+    val client = HttpClient(CIO) {
+
+        install(HttpTimeout) {
+            requestTimeoutMillis = 10000 // 10 sekunder
+        }
+    }
 
     KtorServer.create(factory = Netty, port = 8086) {
         configureRouting(config, client)
