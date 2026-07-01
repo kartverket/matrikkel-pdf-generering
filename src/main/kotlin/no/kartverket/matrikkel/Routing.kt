05ba9843d50a331f8ac3a091ebec403546dc44ae
fredrir <fredrik.hansteen@kartverket.no>
2026-07-01 15:43:14 +0200

    fjernet fra routing

--- src/main/kotlin/no/kartverket/matrikkel/Routing.kt
+++ src/main/kotlin/no/kartverket/matrikkel/Routing.kt
@@ -2,14 +2,12 @@ package no.kartverket.matrikkel
 
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import no.kartverket.matrikkel.routes.convertRoutes
 import no.kartverket.matrikkel.routes.internalRoutes
 import no.kartverket.matrikkel.routes.redirectRoutes
 
 fun Application.configureRouting() {
     routing {
         internalRoutes()
-        convertRoutes()
         redirectRoutes()
     }
 }
