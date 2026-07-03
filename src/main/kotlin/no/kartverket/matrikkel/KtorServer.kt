93d5a4e675b828e6932a54ca646f643e94100635
fredrir <fredrik.hansteen@kartverket.no>
2026-07-03 09:23:10 +0200

    splitte ut loggføring

--- src/main/kotlin/no/kartverket/matrikkel/KtorServer.kt
+++ src/main/kotlin/no/kartverket/matrikkel/KtorServer.kt
@@ -2,10 +2,13 @@ package no.kartverket.matrikkel
 
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
+import org.slf4j.LoggerFactory
 import kotlin.time.Duration.Companion.seconds
 
 
 object KtorServer {
+
+    private val logger = LoggerFactory.getLogger(KtorServer::class.java)
 
     fun <TEngine : ApplicationEngine, TConfiguration : ApplicationEngine.Configuration> create(
         factory: ApplicationEngineFactory<TEngine, TConfiguration>,
@@ -13,23 +16,30 @@ object KtorServer {
         configure: TConfiguration.() -> Unit = {},
         application: Application.() -> Unit,
     ): EmbeddedServer<TEngine, TConfiguration> {
-        return embeddedServer(
-            factory,
-            serverConfig(applicationEnvironment()) {
-                module {
-                    application()
-                }
-            },
-            configure = {
-                connectors.add(
-                    EngineConnectorBuilder().apply {
-                        this.port = port
-                    },
-                )
-                shutdownGracePeriod = 5.seconds.inWholeMilliseconds
-                shutdownTimeout = 30.seconds.inWholeMilliseconds
-                configure()
-            },
-        )
+        val server =
+            embeddedServer(
+                factory,
+                serverConfig(applicationEnvironment()) {
+                    module {
+                        application()
+                    }
+                },
+                configure = {
+                    connectors.add(
+                        EngineConnectorBuilder().apply {
+                            this.port = port
+                        },
+                    )
+                    shutdownGracePeriod = 5.seconds.inWholeMilliseconds
+                    shutdownTimeout = 30.seconds.inWholeMilliseconds
+                    configure()
+                },
+            )
+
+        server.addShutdownHook {
+            logger.info("Shutdown hook called, shutting down gracefully")
+        }
+
+        return server
     }
 }
