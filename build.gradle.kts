d019f900e83036eaf8860eb4405c1d2971499bd1
Ola Lømo Ellingsen <122975059+olalellingsen@users.noreply.github.com>
2026-07-07 16:28:11 +0200

    Update build.gradle.kts
    Co-authored-by: Fredrik Hansteen <fredrik.hansteen@kartverket.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ dependencies {
     implementation(ktorLibs.server.callId)
     implementation(ktorLibs.server.callLogging)
     implementation(ktorLibs.server.contentNegotiation)
-    implementation(ktorLibs.client.contentNegotiation)
     implementation(ktorLibs.server.core)
     implementation(ktorLibs.server.cors)
     implementation(ktorLibs.server.forwardedHeader)
