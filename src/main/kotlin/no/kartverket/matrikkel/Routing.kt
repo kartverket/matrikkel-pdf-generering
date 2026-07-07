dfd8b16eb3dc4d4f1c21450ae9f4fa161a4f1abb
Ola Lømo Ellingsen <122975059+olalellingsen@users.noreply.github.com>
2026-07-07 16:25:42 +0200

    Update src/main/kotlin/no/kartverket/matrikkel/Routing.kt
    Co-authored-by: Fredrik Hansteen <fredrik.hansteen@kartverket.no>

--- src/main/kotlin/no/kartverket/matrikkel/Routing.kt
+++ src/main/kotlin/no/kartverket/matrikkel/Routing.kt
@@ -7,7 +7,7 @@ import no.kartverket.matrikkel.pdfgen.PdfService
 import no.kartverket.matrikkel.routes.createRoutes
 import no.kartverket.matrikkel.routes.internalRoutes
 
-fun Application.configureRouting(client: FrontendClient, pdfService: PdfService) {
+fun Application.configureRouting(frontendClient: FrontendClient, pdfService: PdfService) {
     routing {
         internalRoutes()
         createRoutes(client, pdfService)
